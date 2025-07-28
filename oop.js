bdeba8f82364fb5c99607b69c63d405f9bbcba66
yhnthk <panthero1601@gmail.com>
2025-07-28 08:03:38 +0700

    Add files via upload

---
+++ oop.js
@@ -0,0 +1,51 @@
+// let car = {
+//     name: "fiat",
+//     model: 500,
+//     weight: "850kg",
+//     color: "white",
+
+//     start: function() {
+//         console.log("mobil dinyalakan.");
+//     },
+//     drive: function(){
+//         console.log("mobil berjalan.");
+//     },
+//     brake: function(){
+//         console.log("mobil mengerem.");
+//     },
+//     stop: function(){
+//         console.log("mobil berhenti.");
+//     }
+// }
+
+// console.log(car.name);
+// console.log(car.color);
+
+// car.start();
+// car.drive();
+let sepeda = {
+    name: "polygon",
+    gear: 21,
+    weight: "850kg",
+    color: "white",
+
+    start: function() {
+        console.log("sepeda mulai dikayuh.");
+    },
+    drive: function(){
+        console.log("mobil berjalan.");
+    },
+    brake: function(){
+        console.log("sepeda mengerem.");
+    },
+    stop: function(){
+        console.log("mobil berhenti.");
+    }
+}
+
+console.log(sepeda.name);
+console.log(sepeda.color);
+console.log(sepeda.gear);
+
+sepeda.start();
+sepeda.brake();
