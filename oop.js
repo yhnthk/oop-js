6f40f6a43f1f881c7bd294a259df108bdae73af3
yhnthk <panthero1601@gmail.com>
2025-07-30 10:18:55 +0700

    Add files via upload

--- oop.js
+++ oop.js
@@ -23,29 +23,74 @@
 
 // car.start();
 // car.drive();
-let sepeda = {
-    name: "polygon",
-    gear: 21,
-    weight: "850kg",
-    color: "white",
+// let sepeda = {
+//     name: "polygon",
+//     gear: 21,
+//     weight: "850kg",
+//     color: "white",
 
-    start: function() {
-        console.log("sepeda mulai dikayuh.");
-    },
-    drive: function(){
-        console.log("mobil berjalan.");
-    },
-    brake: function(){
-        console.log("sepeda mengerem.");
-    },
-    stop: function(){
-        console.log("mobil berhenti.");
+//     start: function() {
+//         console.log("sepeda mulai dikayuh.");
+//     },
+//     drive: function(){
+//         console.log("mobil berjalan.");
+//     },
+//     brake: function(){
+//         console.log("sepeda mengerem.");
+//     },
+//     stop: function(){
+//         console.log("mobil berhenti.");
+//     }
+// }
+
+// console.log(sepeda.name);
+// console.log(sepeda.color);
+// console.log(sepeda.gear);
+
+// sepeda.start();
+// sepeda.brake();
+
+// function sepeda(merk, gear,color, idk) {
+//     this.merk = merk;
+//     this.gear = gear;
+//     this.color = color;
+//     this.idk = idk;
+
+//     this.start = function() {
+//         console.log(`${this.merk}gatau merek sepeda.`);
+//     };
+//     this.drive = function() {
+//         console.log(`${this.gear}219.`);
+//     };
+// }
+// let sepeda1 = new sepeda("honda. saya tidak ","idk", "green", "idk bro")
+// let sepeda2 = new sepeda("jumlah giginya ada ","jumlah giginya ada ", "green", "idk vro")
+
+// sepeda1.start();
+// sepeda2.drive();
+
+class Student {
+    constructor(name, jurusan, angkatan) {
+        this.name = name;
+        this.jurusan = jurusan;
+        this.angkatan = angkatan;
+    }
+
+    introduce() {
+        console.log(`Halo, nama saya ${this.name}, dari jurusan ${this.jurusan}, angkatan ${this.angkatan}.`);
+    }
+
+    isSenior() {
+        // misal senior adalah angkatan <= 2
+        return this.angkatan <= 2;
     }
 }
 
-console.log(sepeda.name);
-console.log(sepeda.color);
-console.log(sepeda.gear);
+let student1 = new Student("shrek", "RPL", 1);
+let student2 = new Student("mr.crab", "RPL", 2);
 
-sepeda.start();
-sepeda.brake();
+student1.introduce();
+console.log(`Apakah ${student1.name} senior?`, student1.isSenior());
+
+student2.introduce();
+console.log(`Apakah ${student2.name} senior?`, student2.isSenior());
